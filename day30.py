78dcce74d0611080e6d0fa5893853c4a36d91352
Iann Proano <proano.i@northeastern.edu>
2025-11-17 14:34:06 -0800

    done for today RELEASE ME

--- day30.py
+++ day30.py
@@ -10,5 +10,15 @@ orders = pd.DataFrame(data)
 high_orders = orders[orders['quantity'] >= 2]
 takeout_orders = orders[orders['order_type'] == 'takeout']
 
+data1 = {
+    "workout": ["Upper", "Lower", "Legs", "Swimming"],
+    "type": ["Weights", "Weights", "Weights", "Cardio"],
+    "time": [60, 60, 60, 30], 
 
-print(orders.iloc[0][0])
+}
+
+exercise_data = pd.DataFrame(data1)
+
+morethan_45 = exercise_data[exercise_data["time"] >= 45]
+
+
